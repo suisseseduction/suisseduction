7e86bcd5415a6329b77c206e4866e59bc906dcd2
martinpcs21 <suisseduction@gmail.com>
2022-11-21 15:59:42 +0100

    First commit

---
+++ js/change_language.js
@@ -0,0 +1,22 @@
+//console.log(navigator.language)
+//var redirected = false;
+
+/*if ((navigator.language != 'es-ES') && (window.location.pathname != '/en') && (redirected == false)) {
+    redirected = true;
+    window.location.pathname = '/en';
+}*/
+
+$('#SpanishLanguage').click(function () {
+    window.location.href = 'index.html';
+    //console.log('Spanish');
+});
+
+$('#EnglishLanguage').click(function () {
+    window.location.href = 'indexen.html';
+    //console.log('English');
+});
+
+$('#FrenchLanguage').click(function () {
+    window.location.href = 'index.html';
+    //console.log('English');
+});
